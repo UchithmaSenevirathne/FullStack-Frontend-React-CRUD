9e462f3d2fb793e6bb53f3e2dcd2f797ce987e06
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:02:46 +0530

    create add user form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const { id } = useParams();
 
