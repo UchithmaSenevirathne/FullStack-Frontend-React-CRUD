d7c7a1d906791074a9feedcd83dce65daca52d65
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:07:35 +0530

    create variable to show users

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -2,4 +2,12 @@ import axios from "axios";
 import React, { useEffect,useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-export default function ViewUser() {}
+export default function ViewUser() {
+    const [user, setUser] = useState({
+        name: "",
+        username: "",
+        email: "",
+      });
+    
+      const { id } = useParams();
+}
