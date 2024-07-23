7e6764a566354d187bb833610b33cad814b01dbc
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:00:17 +0530

    create variable to navigate home

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,3 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
+export default function AddUser() {
+    let navigate = useNavigate();
+}
