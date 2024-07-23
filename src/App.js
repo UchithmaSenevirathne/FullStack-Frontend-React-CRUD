075da6633334ae577bd778fe29dc941104aaa2da
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:10:54 +0530

    add route paths

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
       <Router>
       <Navbar />
 <Routes>
-  
+<Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
 </Routes>
       </Router>
     </div>
