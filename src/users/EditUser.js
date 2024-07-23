74222af4ef2f17600a728409beb22c18e9bdeea2
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:04:12 +0530

    create variable same as add user to store updated users

--- src/users/EditUser.js
+++ src/users/EditUser.js
@@ -2,4 +2,15 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
-export default function EditUser() {}
+export default function EditUser() {
+
+    let navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+}
