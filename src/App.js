490b7449d900806c9be6afe7426b04c92975b598
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:10:14 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
+import { Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+
+      </Router>
     </div>
   );
 }
