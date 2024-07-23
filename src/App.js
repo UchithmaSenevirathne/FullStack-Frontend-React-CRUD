46d46821bf2b6c42c6490af1f00126c6f6346a0f
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-23 21:12:11 +0530

    import

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
-import { Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar />
-<Routes>
-<Route exact path="/" element={<Home />} />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/adduser" element={<AddUser />} />
           <Route exact path="/edituser/:id" element={<EditUser />} />
           <Route exact path="/viewuser/:id" element={<ViewUser />} />
-</Routes>
+        </Routes>
       </Router>
     </div>
   );
